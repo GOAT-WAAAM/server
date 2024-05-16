c167e01b0223ec1ee2f2fae60210a98c2c2be73f
encoreJeong <zzaibean@gmail.com>
2024-05-16 13:32:47 +0900

    test: AuthServiceTest 추가

--- src/main/java/com/goat/server/auth/application/AuthService.java
+++ src/main/java/com/goat/server/auth/application/AuthService.java
@@ -3,7 +3,6 @@ package com.goat.server.auth.application;
 import com.goat.server.auth.dto.response.ReIssueSuccessResponse;
 import com.goat.server.global.util.JwtTokenProvider;
 import com.goat.server.global.util.filter.UserAuthentication;
-import com.goat.server.mypage.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
