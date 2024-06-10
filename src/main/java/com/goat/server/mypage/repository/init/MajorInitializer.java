1da20ae70055aeccefee847eab85dd81279d566b
jiwonniy <jiwoni0707@gmail.com>
2024-06-10 21:04:40 +0900

    refactor: 더미 데이터 변경

--- src/main/java/com/goat/server/mypage/repository/init/MajorInitializer.java
+++ src/main/java/com/goat/server/mypage/repository/init/MajorInitializer.java
@@ -3,8 +3,10 @@ package com.goat.server.mypage.repository.init;
 import com.goat.server.global.util.LocalDummyDataInit;
 import com.goat.server.mypage.domain.Major;
 import com.goat.server.mypage.domain.User;
+import com.goat.server.mypage.exception.UserNotFoundException;
+import com.goat.server.mypage.exception.errorcode.MypageErrorCode;
 import com.goat.server.mypage.repository.MajorRepository;
-import com.goat.server.subject.domain.Subject;
+import com.goat.server.mypage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -14,32 +16,14 @@ import org.springframework.core.annotation.Order;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.goat.server.mypage.repository.init.UserInitializer.DUMMY_GUEST;
-import static com.goat.server.mypage.repository.init.UserInitializer.DUMMY_MYPAGEUSER;
-
-
 @Slf4j
 @RequiredArgsConstructor
 @LocalDummyDataInit
-@Order(4)
+@Order(3)
 public class MajorInitializer implements ApplicationRunner {
 
     private final MajorRepository majorRepository;
-
-    public static final Major DUMMY_MAJOR1 = Major.builder()
-            .majorName("dummyMajor1")
-            .user(DUMMY_GUEST)
-            .build();
-
-    public static final Major DUMMY_MAJOR2 = Major.builder()
-            .majorName("dummyMajor2")
-            .user(DUMMY_MYPAGEUSER)
-            .build();
-
-    public static final Major DUMMY_MAJOR3 = Major.builder()
-            .majorName("dummyMajor3")
-            .user(DUMMY_MYPAGEUSER)
-            .build();
+    private final UserRepository userRepository;
 
     @Override
     public void run(ApplicationArguments args) {
@@ -47,11 +31,45 @@ public class MajorInitializer implements ApplicationRunner {
         if (majorRepository.count() > 0) {
             log.info("[Major]더미 데이터 존재");
         } else {
+            User admin = userRepository.findByEmail("adminEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
+            User user = userRepository.findByEmail("userEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
+            User guest = userRepository.findByEmail("guestEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
+
             List<Major> majorList = new ArrayList<>();
 
-            majorList.add(DUMMY_MAJOR1);
-            majorList.add(DUMMY_MAJOR2);
-            majorList.add(DUMMY_MAJOR3);
+            Major DUMMY_ADMIN_MAJOR1 = Major.builder()
+                    .majorName("dummyMajor1")
+                    .user(user)
+                    .build();
+
+            Major DUMMY_USER_MAJOR1 = Major.builder()
+                    .majorName("dummyMajor1")
+                    .user(user)
+                    .build();
+
+            Major DUMMY_USER_MAJOR2 = Major.builder()
+                    .majorName("dummyMajor2")
+                    .user(user)
+                    .build();
+
+            Major DUMMY_USER_MAJOR3 = Major.builder()
+                    .majorName("dummyMajor3")
+                    .user(user)
+                    .build();
+
+            Major DUMMY_GUEST_MAJOR1 = Major.builder()
+                    .majorName("dummyMajor1")
+                    .user(user)
+                    .build();
+
+            majorList.add(DUMMY_ADMIN_MAJOR1);
+            majorList.add(DUMMY_USER_MAJOR1);
+            majorList.add(DUMMY_USER_MAJOR2);
+            majorList.add(DUMMY_USER_MAJOR3);
+            majorList.add(DUMMY_GUEST_MAJOR1);
 
             majorRepository.saveAll(majorList);
         }
--- src/main/java/com/goat/server/notification/repository/init/NotificationSettingInitializer.java
+++ src/main/java/com/goat/server/notification/repository/init/NotificationSettingInitializer.java
@@ -1,6 +1,10 @@
 package com.goat.server.notification.repository.init;
 
 import com.goat.server.global.util.LocalDummyDataInit;
+import com.goat.server.mypage.domain.User;
+import com.goat.server.mypage.exception.UserNotFoundException;
+import com.goat.server.mypage.exception.errorcode.MypageErrorCode;
+import com.goat.server.mypage.repository.UserRepository;
 import com.goat.server.notification.domain.NotificationSetting;
 import com.goat.server.notification.repository.NotificationSettingRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,33 +16,54 @@ import org.springframework.core.annotation.Order;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.goat.server.mypage.repository.init.UserInitializer.DUMMY_MYPAGEUSER;
 
 @Slf4j
 @RequiredArgsConstructor
 @LocalDummyDataInit
-@Order(5)
+@Order(4)
 public class NotificationSettingInitializer implements ApplicationRunner {
 
     private final NotificationSettingRepository notiSettingRepository;
-
-    public static final NotificationSetting DUMMY_SETTING = NotificationSetting.builder()
-            .user(DUMMY_MYPAGEUSER)
-            .isCommentNoti(true)
-            .isPostNoti(true)
-            .isReviewNoti(true)
-            .build();
-
+    private final UserRepository userRepository;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
 
         if (notiSettingRepository.count() > 0) {
             log.info("[Notification Setting]더미 데이터 존재");
         } else {
-            List<NotificationSetting> settingList = new ArrayList<>();
+            User admin = userRepository.findByEmail("adminEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
+            User user = userRepository.findByEmail("userEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
+            User guest = userRepository.findByEmail("guestEmail")
+                    .orElseThrow(() -> new UserNotFoundException(MypageErrorCode.USER_NOT_FOUND));
 
-            settingList.add(DUMMY_SETTING);
+            List<NotificationSetting> settingList = new ArrayList<>();
+            NotificationSetting DUMMY_NOTISETTING1 = NotificationSetting.builder()
+                    .user(admin)
+                    .isCommentNoti(false)
+                    .isPostNoti(false)
+                    .isReviewNoti(true)
+                    .build();
+
+            NotificationSetting DUMMY_NOTISETTING2 = NotificationSetting.builder()
+                    .user(user)
+                    .isCommentNoti(true)
+                    .isPostNoti(true)
+                    .isReviewNoti(true)
+                    .build();
+
+            NotificationSetting DUMMY_NOTISETTING3 = NotificationSetting.builder()
+                    .user(guest)
+                    .isCommentNoti(false)
+                    .isPostNoti(false)
+                    .isReviewNoti(false)
+                    .build();
+
+            settingList.add(DUMMY_NOTISETTING1);
+            settingList.add(DUMMY_NOTISETTING2);
+            settingList.add(DUMMY_NOTISETTING3);
 
             notiSettingRepository.saveAll(settingList);
         }
