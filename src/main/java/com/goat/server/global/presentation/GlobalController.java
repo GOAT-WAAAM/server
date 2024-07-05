b0461598f8986611b6f5f4034a38ab53f0c8e93f
LDY <leedy3838@naver.com>
2024-07-05 10:35:22 +0900

    feat: Directory 조회 설명 추가 #23

---
+++ src/main/java/com/goat/server/global/presentation/GlobalController.java
@@ -0,0 +1,31 @@
+package com.goat.server.global.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "GlobalController", description = "전체 설정을 위해서 필요한 api")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/goat/global")
+public class GlobalController {
+
+    @Value("${server.env}")
+    private String env;
+
+    @Operation(summary = "health check", description = "cicd를 위한 health check api")
+    @GetMapping("/health-check")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(env);
+    }
+}
