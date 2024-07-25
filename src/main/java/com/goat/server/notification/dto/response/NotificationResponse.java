034b00c937628fb94d11634bd62e929c14a8d924
encoreJeong <zzaibean@gmail.com>
2024-07-25 12:51:23 +0900

    update: 알림 목록 조회시, 해당 알림이 며칠전에 왔는지에 대한 정보도 포함하도록 수정

--- src/main/java/com/goat/server/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/goat/server/notification/dto/response/NotificationResponse.java
@@ -3,6 +3,8 @@ package com.goat.server.notification.dto.response;
 import com.goat.server.notification.domain.Notification;
 import lombok.Builder;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Builder
@@ -14,15 +16,39 @@ public record NotificationResponse(
             Long notificationId,
             Long reviewId,
             String reviewBody,
+            String whenItPushed,
             Boolean isRead
     ){
         public static NotificationComponentResponse from(Notification notification) {
+
+            String whenItPushed = calculateWhenItPushed(notification);
+
             return NotificationComponentResponse.builder()
                     .notificationId(notification.getNoti_id())
                     .reviewId(notification.getReview().getId())
                     .reviewBody(notification.getContent())
+                    .whenItPushed(whenItPushed)
                     .isRead(notification.getIsRead())
                     .build();
+        }
+
+        private static String calculateWhenItPushed(Notification notification) {
+            LocalDate currentDate = LocalDate.now();
+            LocalDate createdDate = notification.getCreatedDate().toLocalDate();
+            long daysBetween = ChronoUnit.DAYS.between(createdDate, currentDate);
+            long hoursBetween = ChronoUnit.HOURS.between(createdDate, currentDate);
+
+            String whenItPushed = null;
+            if (daysBetween == 0) {
+                if(hoursBetween == 0) {
+                    whenItPushed = "방금 전";
+                } else {
+                    whenItPushed = hoursBetween + "시간 전";
+                }
+            } else {
+                whenItPushed = daysBetween + "일 전";
+            }
+            return whenItPushed;
         }
     }
 
@@ -31,4 +57,6 @@ public record NotificationResponse(
                 .notifications(notifications)
                 .build();
     }
+
+
 }
